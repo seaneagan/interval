a89eb9b8fe19dd33590603ea91aa7853bd944b70
Ilya Gyrdymov <ilgyrd@gmail.com>
2019-03-28 22:41:58 +0200

    readme updated

--- example/main.dart
+++ example/main.dart
@@ -3,9 +3,9 @@ import 'package:xrange/range.dart';
 void main() {
   final date1 = DateTime(2015);
   final date2 = DateTime(2021);
-  final activeDates = Range<DateTime>.closed(date1, date2);
+  final dates = Range<DateTime>.closed(date1, date2);
 
-  if (activeDates.contains(DateTime.now())) {
+  if (dates.contains(DateTime.now())) {
     print('Hi, contemporary!');
   } else {
     print('Apparently, you are from the future!');
